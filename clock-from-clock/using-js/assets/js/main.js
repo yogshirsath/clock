fc1234e19a40d269146d7b10a2618100cef578fc
Yogesh Shirsath <yogshirsath@hotmail.com>
2020-02-24 23:19:30 +0530

    Added clock using clock and js

---
+++ clock-from-clock/using-js/assets/js/main.js
@@ -0,0 +1,254 @@
+var digits = {
+    "0": [{
+        "deg": [0, 90],
+        "index": [1, 6]
+    }, {
+        "deg": [0, 180],
+        "index": [2, 3, 22, 23]
+    }, {
+        "deg": [90, 180],
+        "index": [4, 7]
+    }, {
+        "deg": [90, 270],
+        "index": [5, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 20]
+    }, {
+        "deg": [180, 270],
+        "index": [19, 24]
+    }, {
+        "deg": [270, 0],
+        "index": [18, 21]
+    }],
+    "1": [{
+        "deg": [0, 90],
+        "index": [1, 17]
+    }, {
+        "deg": [0, 180],
+        "index": [2, 22, 23]
+    }, {
+        "deg": [90, 180],
+        "index": [3, 20, 6]
+    }, {
+        "deg": [90, 270],
+        "index": [7, 10, 11, 14, 15]
+    }, {
+        "deg": [180, 270],
+        "index": [18, 24]
+    }, {
+        "deg": [270, 0],
+        "index": [5, 19, 21]
+    }, {
+        "deg": [135, 135],
+        "index": [4, 8, 9, 12, 13, 16]
+    }],
+    "2": [{
+        "deg": [0, 90],
+        "index": [1, 9, 14]
+    }, {
+        "deg": [0, 180],
+        "index": [2, 3, 6, 10, 15, 19, 22, 23]
+    }, {
+        "deg": [90, 180],
+        "index": [4, 7, 20]
+    }, {
+        "deg": [90, 270],
+        "index": [8, 12, 13, 17]
+    }, {
+        "deg": [180, 270],
+        "index": [11, 16, 24]
+    }, {
+        "deg": [270, 0],
+        "index": [5, 18, 21]
+    }],
+    "3": [{
+        "deg": [0, 90],
+        "index": [1, 9, 17]
+    }, {
+        "deg": [0, 180],
+        "index": [2, 3, 6, 10, 14, 18, 22, 23]
+    }, {
+        "deg": [90, 180],
+        "index": [4, 7, 15] 
+    }, {
+        "deg": [90, 270],
+        "index": [8, 12, 16, 20]
+    }, {
+        "deg": [180, 270],
+        "index": [11, 19, 24]
+    }, {
+        "deg": [270, 0],
+        "index": [5, 13, 21]
+    }],
+    "4": [{
+        "deg": [0, 90],
+        "index": [1, 3]
+    }, {
+        "deg": [0, 180],
+        "index": [14]
+    }, {
+        "deg": [90, 180],
+        "index": [2, 4, 15]
+    }, {
+        "deg": [90, 270],
+        "index": [5, 6, 7, 8, 9, 12, 16, 19, 20]
+    }, {
+        "deg": [180, 270],
+        "index": [11, 24]
+    }, {
+        "deg": [270, 0],
+        "index": [10, 13, 23]
+    }, {
+        "deg": [135, 135],
+        "index": [17, 18, 21, 22]
+    }],
+    "5": [{
+        "deg": [0, 90],
+        "index": [1, 6, 17]
+    }, {
+        "deg": [0, 180],
+        "index": [2, 3, 7, 11, 14, 18, 22, 23]
+    }, {
+        "deg": [90, 180],
+        "index": [4, 12, 15]
+    }, {
+        "deg": [90, 270],
+        "index": [5, 9, 16, 20]
+    }, {
+        "deg": [180, 270],
+        "index": [8, 19, 24]
+    }, {
+        "deg": [270, 0],
+        "index": [10, 13, 21]
+    }],
+    "6": [{
+        "deg": [0, 90],
+        "index": [1, 6, 14]
+    }, {
+        "deg": [0, 180],
+        "index": [2, 3, 7, 11, 22, 23]
+    }, {
+        "deg": [90, 180],
+        "index": [4, 12, 15]
+    }, {
+        "deg": [90, 270],
+        "index": [5, 9, 13, 16, 17, 20]
+    }, {
+        "deg": [180, 270],
+        "index": [8, 19, 24]
+    }, {
+        "deg": [270, 0],
+        "index": [10, 18, 21]
+    }],
+    "7": [{
+        "deg": [0, 90],
+        "index": [1] 
+    }, {
+        "deg": [0, 180],
+        "index": [2, 3, 6]
+    }, {
+        "deg": [90, 180],
+        "index": [4, 7]
+    }, {
+        "deg": [90, 270],
+        "index": [8, 11, 12, 15, 16, 19, 20]
+    }, {
+        "deg": [180, 270],
+        "index": [24]
+    }, {
+        "deg": [270, 0],
+        "index": [5, 23]
+    }, {
+        "deg": [135, 135],
+        "index": [9, 10, 13, 14, 17, 18, 21, 22]
+    }],
+    "8": [{
+        "deg": [0, 90],
+        "index": [1, 6, 14]
+    }, {
+        "deg": [0, 180],
+        "index": [2, 3, 22, 23]
+    }, {
+        "deg": [90, 180],
+        "index": [4, 7, 15]
+    }, {
+        "deg": [90, 270],
+        "index": [5, 8, 9, 12, 13, 16, 17, 20]
+    }, {
+        "deg": [180, 270],
+        "index": [11, 19, 24]
+    }, {
+        "deg": [270, 0],
+        "index": [10, 18, 21]
+    }],
+    "9": [{
+        "deg": [0, 90],
+        "index": [1, 6, 17]
+    }, {
+        "deg": [0, 180],
+        "index": [2, 3, 14, 18, 22, 23]
+    }, {
+        "deg": [90, 180],
+        "index": [4, 7, 15]
+    }, {
+        "deg": [90, 270],
+        "index": [5, 8, 9, 12, 16, 20]
+    }, {
+        "deg": [180, 270],
+        "index": [11, 19, 24]
+    }, {
+        "deg": [270, 0],
+        "index": [10, 13, 21]
+    }]
+};
+function getDigit(type, length) {
+    var digit = $('<div class="digit" data-' + type + '="' + length + '"></div>');
+    for(var i = 0; i < 24; i++) {
+        var ticker = $('<div class="ticker"></div>');
+        ticker.append('<div class="tic t1"></div>');
+        ticker.append('<div class="tic t2"></div>');
+        digit.append(ticker);
+    }
+    return digit;
+}
+function generateClock(container) {
+    var times = ['hours', 'minutes', 'seconds'];
+    var separators = 2;
+    times.forEach(function(time) {
+        $(container).append(getDigit(time, 'tenth'));
+        $(container).append(getDigit(time, 'digit'));
+        if (separators != 0) {
+            $(container).append($('<div></div>').addClass('separator'));
+            separators--;
+        }
+    });
+}
+function applyNumber(element, digit) {
+    var parameters = digits[digit];
+    parameters.forEach(function(rotation) {
+        if (rotation.index.length != 0) {
+            rotation.index.forEach(function(miniclock) {
+                miniclock = $(element).find('.ticker:nth-child(' + miniclock + ')');
+                miniclock.find('.tic.t1').css('transform', 'rotate(' + rotation.deg[0] + 'deg)');
+                miniclock.find('.tic.t2').css('transform', 'rotate(' + rotation.deg[1] + 'deg)');
+            });
+        }
+    });
+}
+function applyTime(prefix, time) {
+    var digit = time % 10;
+    applyNumber($('[data-' + prefix + '="digit"]'), digit);
+    var tenth = Math.floor(time / 10);
+    applyNumber($('[data-' + prefix + '="tenth"]'), tenth);
+}
+function updateClock() {
+    var date = new Date();
+    applyTime('seconds', date.getSeconds());
+    applyTime('minutes', date.getMinutes());
+    applyTime('hours', date.getHours());
+}
+$(document).ready(function() {
+    generateClock($('.clock'));
+    updateClock();
+    setInterval(function() {
+        updateClock();
+    }, 1000);
+});
